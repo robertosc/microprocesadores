24dd290d3afc76a32b69c75948ee0e0424a7897e
Roberto Sánchez <roberto.sanchezcardenas@ucr.ac.cr>
2021-02-18 22:52:22 -0600

    ok

---
+++ tarea6/tarea6.asm
@@ -0,0 +1,210 @@
+
+#include registers.inc
+
+EOM:            EQU $00 ;end of message
+CR:             EQU $0D ;carriage return
+LF:             EQU $0A ;line feed
+NP:             EQU $0C ;new page
+SUB:            EQU $1A ;control substitute
+
+;Definicion de vectores:
+                Org $FFD2
+                Dw ATD0_ISR ;direccion de la subrutina de servicio a interrupcion ATD0.
+                ;Org $FFD4
+                ;Dw SCI_ISR ;direccion de la subrutina de servicio a interrupcion SCI1.
+                ;Org $FFF0
+                ;Dw RTI_ISR  ;direccion de la subrutina de servicio a interrupcion RTI.
+
+;Estructuras de datos:
+               Org $1010
+
+NIVEL_PROM: Ds 2
+BCD_H:          ds 1
+BCD_L:          ds 1
+LOW:            ds 1
+VOLUMEN:        ds 1
+
+
+;Mensajes:
+MENSAJE:        Db SUB
+                Fcc "                         UNIVERSIDAD DE COSTA RICA"
+                Db CR,LF
+                Fcc "                      ESCUELA DE INGENIERIA ELECTRICA"
+                Db CR,LF
+                Fcc "                             MICROPROCESADORES"
+                Db CR,LF
+                Fcc "                                   IE0623"
+                Db CR,LF,CR,LF
+                Fcc "                            VOLUMEN CALCULADO: "
+CENTENAS:       Ds 1
+DECENAS:        Ds 1
+UNIDADES:       Ds 1
+                Fcc " m3."
+                Db CR,LF,EOM
+
+MSG_ALARM:      Db CR,LF,CR,LF
+                Fcc "                    Alarma: El Nivel esta Bajo."
+                Db EOM
+
+MSG_FULL:       Fcc "                    Tanque vaciando, Bomba Apagada."
+                Db EOM
+;*******************************************************************************
+;                             Programa principal
+;*******************************************************************************
+;------------------------------------------------------------------------------
+;                          Configuracion del hardware
+;------------------------------------------------------------------------------
+    Org $2000
+    
+    
+    ;Configuracion del ATD0:
+    Movb #$C2,ATD0CTL2 ;habilita convertidor ATD0, borrado rapido de banderas y las interrupciones.
+    Ldaa #160
+CONFIG_ATD:
+    Dbne A,CONFIG_ATD ;3ciclosCLK * 160cuentas * (1/48 MHz) = 10 us. Tiempo requerido para que inicie el ATD.
+    Movb #$30,ATD0CTL3 ;ciclo de 6 conversiones
+    Movb #$10,ATD0CTL4 ;conversion a 10 bits, tiempo final de muestreo es 2 periodos, f_sample = 700 kHz (PRS~16)
+    Movb #$80,ATD0CTL5 ;justifica a la derecha. Sin signo. No multiplexacion. Canal 7 es el del pot. Escritura inicia ciclo.
+    
+
+
+
+    Cli
+;------------------------------------------------------------------------------
+;                       Inicializacion de variables
+;------------------------------------------------------------------------------
+    LDS #$4000  ;inicializa el stack
+    Ldx #0
+    ;MOVB #100,CONT_RTI
+    ;CLR FLAGS
+;------------------------------------------------------------------------------
+MAIN_LOOP:
+    ;JSR CALCULO
+    ;JSR BIN_BCD
+    ;JSR BCD_ASCII_999
+    ;JSR DETERMINE_FLAGS
+    BRA MAIN_LOOP
+;*******************************************************************************
+
+BIN_BCD:
+                ldx #15
+                     clra
+                    ldab VOLUMEN
+
+                    clr BCD_H
+                    clr BCD_L
+            
+LOOP_BINBCD:
+                    lsld       ;se realiza el desplazamiento
+		rol BCD_L
+                    rol BCD_H
+                     pshd
+                    ldaa #$0F
+                    anda BCD_L
+                    cmpa #5
+                    blo UNIDADES_CONV
+                    adda #3
+UNIDADES_CONV:
+                    staa LOW
+                    ldaa #$F0 ;mascara
+                    anda BCD_L ;aplica mascara
+                    cmpa #$50  ;compara con 5
+                    blo DECENAS_CONV
+                    adda #$30
+DECENAS_CONV:
+                    adda LOW
+                    staa BCD_L
+                    ldaa #$0F ;mascara
+                    anda BCD_H ;se obtienen las centenas
+                    cmpa #5
+                    blo CENTENAS_CONV
+                    adda #3
+CENTENAS_CONV:
+                    staa LOW
+                    ldaa #$F0 ;mascara
+                    anda BCD_H ;se obtienen los miles
+                    cmpa #$50
+                    blo MILES_CONV
+                    adda #$30
+MILES_CONV:
+                    adda LOW
+                    staa BCD_H
+                	puld
+		dex
+                bne LOOP_BINBCD
+                lsld
+                    rol BCD_L
+                    rol BCD_H
+                     rts
+
+BCD_ASCII:
+    ldaa #$0F ;mascara
+    anda BCD_L ;unidades
+    adda #$30 ;conversion, basat con sumar $30
+    staa UNIDADES
+    ldab #$F0 ;mascara
+    andb BCD_L ;decenas
+    lsrb
+    lsrb
+    lsrb
+    lsrb ;desplazamos
+    addb #$30
+    stab DECENAS
+    ldaa #$0F
+    anda BCD_H ;centenas
+    adda #$30
+    staa CENTENAS
+    rts
+    
+    
+DETERMINAR_NIVEL_ESTADO:
+        ldaa VOLUMEN
+        cmpa #95
+        bhs LED_OFF
+        
+        cmpa #16
+        bls LED_ON
+        
+        cmpa #32
+        bhs NO_PRINT
+        
+        brclr FLAGS,$04
+        bra NO_PRINT
+
+LED_ON:
+	BSET PORTB %00000001 ;enciende la bomba
+	BCLR FLAGS %00000100 ;MSG_SEL <--- 0 para indicar que el mensaje a imprimir es el de alarma
+	BRA PRINT
+
+LED_OFF:
+	BCLR PORTB %00000001 ;se apaga la bomba
+	BSET FLAGS %00000100 ;MSG_SEL <--- 1 para indicar que el mensaje a imprimir es el de tanque lleno
+PRINT:
+	BSET FLAGS %00000010 ;MSG <-- 1 para indicar que hay que imprimir otro mensaje
+ 	rts
+
+NO_PRINT:
+	BCLR FLAGS %00000010 ;MSG <-- 0 para indicar que NO hay que imprimir otro mensaje
+        rts
+
+
+
+;------------------------------------------------------------------------------
+;   Subrutina ATD0_ISR: luego de que el ATD realiza las 6 mediciones al canal 0
+;     esta subruina calcula el promedio de esas mediciones y lo guarda en la
+;     variable NIVEL_PROM. Ademas inicia el siguiente ciclo de conversion.
+;------------------------------------------------------------------------------
+ATD0_ISR:
+    Ldd ADR00H
+    Addd ADR01H
+    Addd ADR02H
+    Addd ADR03H
+    Addd ADR04H
+    Addd ADR05H
+    ;Tenemos en RR1 la suma de los 6 numeros
+    Ldx #6
+    Idiv ;X = D/X para el promedio
+    Stx NIVEL_PROM ;Dado por el enunciado para guardar el promedio
+    Movb #$80,ATD0CTL5 ;escribimos para ques se repita el proceso
+    Rti
+;------------------------------------------------------------------------------
